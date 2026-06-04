2564bab6dd5e8475aeb19ac4223e5b0225184857
Alex Mc <358681+WorldsAreYours@users.noreply.github.com>
2026-06-03 20:30:14 -0500

    1a.3 - silero setup

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import io
 import os
 from dotenv import load_dotenv
 import sounddevice as sd
+import sys; print(sys.path)
 from sounddevice import RawInputStream
 from record.recorder import Recorder
 from record.audiobuffer import AudioBuffer
@@ -11,6 +12,9 @@ load_dotenv()
 buffer = AudioBuffer()
 
 recorder = Recorder(48000, 10, 'float32', callback=buffer)
-
+print(type(recorder.stream))
 with recorder.stream:
     sd.sleep(5000)
+
+if __name__ == "main":
+    recorder.start()
--- src/record/audiobuffer.py
+++ src/record/audiobuffer.py
@@ -1,11 +1,18 @@
+from silero_vad import load_silero_vad, read_audio, get_speech_timestamps
+from librosa import resample
+
 class AudioBuffer:
     def __init__(self):
         self.data = bytearray()
         self.samplerate = 48000
         self.duration = 3
         self.bytes_per_sample = 4
+        model = load_silero_vad()
     
     def __call__(self, indata, frames, time, status):
         self.data.extend(indata)
         if len(self.data) >= self.samplerate * self.duration * self.bytes_per_sample:
+
+            # resample
+            # resample()
             print(f"birdnet is ready for these {len(self.data)} frames")
